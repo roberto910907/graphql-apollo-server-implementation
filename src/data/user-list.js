bbe6deaecb521b44e1f9c4ae09ce3076ab687a98
Roberto Rielo <roberto910907@gmail.com>
2019-07-27 17:37:42 -0500

    Added graphql schema and resolvers

---
+++ src/data/user-list.js
@@ -0,0 +1,16 @@
+export let users = [
+    {
+        id: 1,
+        firstname: 'Jon',
+        lastname: 'Snow',
+    }, {
+        id: 2,
+        firstname: 'Sansa',
+        lastname: 'Stark',
+    },
+    {
+        id: 3,
+        firstname: 'Daenerys',
+        lastname: 'Targaryen',
+    },
+];
--- src/index.js
+++ src/index.js
@@ -1,34 +1,54 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import { users } from './data/user-list';
+import slugify from 'slugify';
 
 const app = express();
 
 const schema = gql`
   type Query {
-    me: User
+    me: User,
+    users: [User!],
+    user(id: ID!): User
   }
 
   type User {
-    username: String!
+    id: ID!,
+    username: String!,
+    firstname: String!,
+    lastname: String!,
+    fullname: String!,
   }
 `;
+
 const resolvers = {
-    Query: {
-        me: () => {
-            return {
-                username: 'Robin Wieruch',
-            };
-        },
+  Query: {
+    me: () => {
+      return {
+        username: 'roberto.rielo.v',
+        fullname: 'Roberto Rielo',
+      };
     },
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+  },
+  User: {
+    fullname: (user) => `${user.firstname} ${user.lastname}`,
+    username: (user) => slugify(`${user.firstname} ${user.lastname}`, { replacement: '.', lower: true }),
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
+  typeDefs: schema,
+  resolvers,
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen({ port: 8000 }, () => {
-    console.log('Apollo Server on http://localhost:8000/graphql');
+  console.log('Apollo Server on http://localhost:8000/graphql');
 });
